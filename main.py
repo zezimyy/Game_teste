a219e34c8870ce361fa136d84c5f47dd863813ec
snoopyyyy <66038323+zezimyy@users.noreply.github.com>
2022-09-07 03:05:40 +0000

    Adicionei moedas aleatórias e movimentação do personagem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pygame.init()
 
@@ -10,13 +11,18 @@ altura = 480
 tela = pygame.display.set_mode((largura, altura))
 
 #movimentação
-x = 0
-y = 0
+x_player = largura / 2
+y_player = altura / 2
+
+#moedaX,y
+x_coin = randint(40, 600)
+y_coin = randint(40, 440)
 
 relogio = pygame.time.Clock()
 
 while True:
-    relogio.tick(60)
+    #fps
+    relogio.tick(15)
     tela.fill((0, 0, 0))
 
     for event in pygame.event.get():
@@ -25,13 +31,27 @@ while True:
             exit()
 
     #Persongem
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 40))
+    player = pygame.draw.rect(tela, (255, 0, 0), (x_player, y_player, 40, 40))
 
     #Moeda
-    pygame.draw.circle(tela, (0, 255, 255), (300, 200), 5)
+    coin = pygame.draw.circle(tela, (0, 255, 255), (x_coin, y_coin), 5)
 
-    y = y + 1
-    if y >= altura:
-        y = 0
+    #esquerda
+    if pygame.key.get_pressed()[K_a]:
+        x_player = x_player - 10
+    #direita
+    if pygame.key.get_pressed()[K_d]:
+        x_player = x_player + 10
+    #cima
+    if pygame.key.get_pressed()[K_w]:
+        y_player = y_player - 10
+    #baixo
+    if pygame.key.get_pressed()[K_s]:
+        y_player = y_player + 10
+
+    #pegar moeda
+    if player.colliderect(coin):
+        x_coin = randint(40, 600)
+        y_coin = randint(40, 440)
 
     pygame.display.update()
